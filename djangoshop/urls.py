d46b489eaed795670e7005b644c90080070e13b4
sergeykibush <sergeykibush@gmail.com>
2018-06-07 01:01:27 +0300

    Hello World and some base settings

--- djangoshop/urls.py
+++ djangoshop/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from shop.views import hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello_world, name='main')
+    path('', hello_world, name='main'),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
